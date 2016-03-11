a38efabf76595fbecfa90f3edcdcb51c1ab4ddd5
timothyhartanto <hartantotimothy@gmail.com>
2016-03-11 23:32:04 +0700

    Update Prize.java

--- Prize.java
+++ Prize.java
@@ -7,6 +7,8 @@ public class Prize {
 	static String file = "";
 	static String[] data;
 	
+	final static int OFS_VOLUME = 3;
+	
 	public static void main(String[] args){
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int i = 0;
@@ -16,6 +18,7 @@ public class Prize {
 				for(int x = 0; x < data.length; x++){
 					files[i][x] = Integer.parseInt(data[x]);
 				}
+				files[i][OFS_VOLUME] = files[i][3] * files [i][4];
 			}catch(Exception e){}
 			i++;
 		}
