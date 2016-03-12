5732c98004077b91b122c74b0aba5c147fb592b4
timothyhartanto <hartantotimothy@gmail.com>
2016-03-12 10:41:22 +0700

    Update Prize.java

--- Prize.java
+++ Prize.java
@@ -3,27 +3,37 @@ package com.redmart.prize;
 import java.io.*;
 
 public class Prize {
-	static int[][] files = new int[2000][6];
-	static String file = "";
-	static String[] data;
+	private final static int OFS_PROD_ID = 0;
+	private final static int OFS_PRICE = 1;
+	private final static int OFS_VOLUME = 2;
+	private final static int OFS_WEIGHT = 3;
+	private final static int PROD_SIZE = 20000;
 	
-	final static int OFS_VOLUME = 3;
-	
+	private static int[][] files = new int[PROD_SIZE][4];
+	//private static String file = "";
+	private static String[] data;
+
 	public static void main(String[] args){
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int i = 0;
+		int i = 0, volume = 0;
+		
 		while(i < 6){
 			try{
 				data = br.readLine().split(",");
-				for(int x = 0; x < data.length; x++){
-					files[i][x] = Integer.parseInt(data[x]);
-				}
-				files[i][OFS_VOLUME] = files[i][3] * files [i][4];
+					
+					files[i][OFS_PROD_ID] = Integer.parseInt(data[OFS_PROD_ID]);
+					files[i][OFS_PRICE] = Integer.parseInt(data[OFS_PRICE]);
+					volume = (Integer.parseInt(data[OFS_VOLUME]) * Integer.parseInt(data[OFS_VOLUME + 1]) *
+							Integer.parseInt(data[OFS_VOLUME + 2])); // get length, width, height
+					files[i][OFS_VOLUME] = volume;
+					files[i][OFS_WEIGHT] = Integer.parseInt(data[OFS_WEIGHT + 2]); // skip offset width and height
+
 			}catch(Exception e){}
 			i++;
 		}
+		
 		for(int y = 0; y < 6; y++){
-			for(int z = 0; z < 6; z++)
+			for(int z = 0; z < 4; z++)
 				System.out.print(files[y][z]);
 			System.out.println();
 		}
